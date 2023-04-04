08ef9592d2e848a87cee933be4fd773e26c5d39c
s183910 <s183910@student.dtu.dk>
2023-04-04 20:00:39 +0200

    colab nb, bounding box txt added

--- validation/bounding_box_saliency_map.py
+++ validation/bounding_box_saliency_map.py
@@ -6,39 +6,43 @@ from torchcam.methods import SmoothGradCAMpp
 from torchvision.io import read_image
 from torchvision.transforms.functional import normalize, resize, to_pil_image
 from torchcam.methods import SmoothGradCAMpp, LayerCAM
+
 # https://frgfm.github.io/torch-cam/methods.html#torchcam.methods.SmoothGradCAMpp
 # I believe this is smoothgrad from the lecture
 from PIL import Image
 from torchvision import transforms
 import cv2
 import glob
+import operator
 
 
 # set up model, pretrained on imagenet
-model = resnet34(weights='DEFAULT').eval()
+model = resnet34(weights="DEFAULT").eval()
 # don't really know what this does (especially the normalize thing - were do the variables come from?
-preprocess = transforms.Compose([
-    transforms.Resize(299),
-    transforms.CenterCrop(299),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
+preprocess = transforms.Compose(
+    [
+        transforms.Resize(299),
+        transforms.CenterCrop(299),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
 
 # to visualize the images in same format
-preprocess_visualise = transforms.Compose([
-    transforms.Resize(299),
-    transforms.CenterCrop(299),
-    transforms.ToTensor()
-])
+preprocess_visualise = transforms.Compose(
+    [transforms.Resize(299), transforms.CenterCrop(299), transforms.ToTensor()]
+)
 
 # loading the test images
-imgs = glob.glob('handful_of_images/*.jpg')
+imgs = glob.glob("handful_of_images/*.jpg")
 len(imgs)
 
-cam_extractor = SmoothGradCAMpp(model,target_layer='layer4')
+cam_extractor = SmoothGradCAMpp(model, target_layer="layer4")
+
+
 def get_heat_map(fileNames, overlay):
-    #getting the saliency maps on the images
-    fig, ax = plt.subplots(2,5, tight_layout=True)
+    # getting the saliency maps on the images
+    fig, ax = plt.subplots(2, 5, tight_layout=True)
     ax = ax.ravel()
     for i, img in enumerate(fileNames):
         input_image = Image.open(img)
@@ -51,10 +55,15 @@ def get_heat_map(fileNames, overlay):
         cams = cam_extractor(out.squeeze(0).argmax().item(), out)
 
         if overlay:
-            result = overlay_mask(to_pil_image(cropped_img), to_pil_image(cams[0].squeeze(0), mode='F'), alpha=0.5)
-            ax[i].imshow(result); ax[i].set_axis_off();
+            result = overlay_mask(
+                to_pil_image(cropped_img), to_pil_image(cams[0].squeeze(0), mode="F"), alpha=0.5
+            )
+            ax[i].imshow(result)
+            ax[i].set_axis_off()
         else:
-            ax[i].imshow(cams[0].squeeze(0)); ax[i].set_axis_off();
+            ax[i].imshow(cams[0].squeeze(0))
+            ax[i].set_axis_off()
+
     plt.show()
 
 
@@ -63,4 +72,29 @@ get_heat_map(imgs, False)
 cam_extractor.remove_hooks()
 
 
+def IoU_saliency(fileNames, id):
+    ax = ax.ravel()
+    for i, img in enumerate(fileNames):
+        input_image = Image.open(img)
+        cropped_img = preprocess_visualise(input_image)
 
+        input_tensor = preprocess(input_image)
+        input_batch = input_tensor.unsqueeze(0)
+
+        out = model(input_batch)
+        cams = cam_extractor(out.squeeze(0).argmax().item(), out)
+
+        # if overlay:
+        #     result = overlay_mask(
+        #         to_pil_image(cropped_img), to_pil_image(cams[0].squeeze(0), mode="F"), alpha=0.5
+        #     )
+        #     ax[i].imshow(result)
+        #     ax[i].set_axis_off()
+        # else:
+        #     ax[i].imshow(cams[0].squeeze(0))
+        #     ax[i].set_axis_off()
+
+        val = []
+        for i in range(0, cams[0].squeeze(0).shape[0]):
+            index, value = max(enumerate(cams[0].squeeze(0)[i]), key=operator.itemgetter(1))
+            val.append(value)
